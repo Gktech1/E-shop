07bf99983b172b9c253908fd6d437f3a5586408e
Olukunle Edun <favourblessing1@gmail.com>
2023-09-29 12:52:43 +0100

    features update on Discount Service

---
+++ src/Discount.API/Program.cs
@@ -0,0 +1,31 @@
+using Discount.API.Extension;
+using Discount.API.Repositories;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+app.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
