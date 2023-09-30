ec5413bad2e511211f5bd81af46e5a7838f03dfb
Olukunle Edun <favourblessing1@gmail.com>
2023-09-30 13:38:15 +0100

    updates

--- src/Services/Discount.API/Program.cs
+++ src/Services/Discount.API/Program.cs
@@ -1,21 +1,13 @@
-using Discount.API.Extension;
-using Discount.API.Repositories;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 var app = builder.Build();
-app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
